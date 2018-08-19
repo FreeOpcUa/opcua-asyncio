1fb105e43403280800467aedc49906d559947036
oroulet <olivier@loke>
2018-08-19 19:41:54 +0200

    new release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="opcua",
-      version="0.98.3",
+      version="0.98.5",
       description="Pure Python OPC-UA client and server library",
       author="Olivier Roulet-Dubonnet",
       author_email="olivier.roulet@gmail.com",
