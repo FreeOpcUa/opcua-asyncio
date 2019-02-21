9f337fbb1c01fced810b5ff544d9e6681d8d9ffa
oroulet <olivier@loke>
2019-02-21 15:41:04 +0100

    complete port of server-example.py and tested with prosys client

--- examples/server-example.py
+++ examples/server-example.py
@@ -70,15 +70,14 @@ async def main():
     # setup our own namespace
     uri = "http://examples.freeopcua.github.io"
     idx = await server.register_namespace(uri)
-    return
 
     # create a new node type we can instantiate in our address space
     dev = await server.nodes.base_object_type.add_object_type(idx, "MyDevice")
-    await dev.add_variable(idx, "sensor1", 1.0).set_modelling_rule(True)
-    await dev.add_property(idx, "device_id", "0340").set_modelling_rule(True)
+    await (await dev.add_variable(idx, "sensor1", 1.0)).set_modelling_rule(True)
+    await (await dev.add_property(idx, "device_id", "0340")).set_modelling_rule(True)
     ctrl = await dev.add_object(idx, "controller")
     await ctrl.set_modelling_rule(True)
-    await ctrl.add_property(idx, "state", "Idle").set_modelling_rule(True)
+    await (await ctrl.add_property(idx, "state", "Idle")).set_modelling_rule(True)
 
     # populating our address space
 
@@ -90,28 +89,28 @@ async def main():
     # create directly some objects and variables
     myobj = await server.nodes.objects.add_object(idx, "MyObject")
     myvar = await myobj.add_variable(idx, "MyVariable", 6.7)
-    myvar.set_writable()    # Set MyVariable to be writable by clients
+    await myvar.set_writable()    # Set MyVariable to be writable by clients
     mystringvar = await myobj.add_variable(idx, "MyStringVariable", "Really nice string")
-    mystringvar.set_writable()    # Set MyVariable to be writable by clients
-    mydtvar = myobj.add_variable(idx, "MyDateTimeVar", datetime.utcnow())
-    mydtvar.set_writable()    # Set MyVariable to be writable by clients
-    myarrayvar = myobj.add_variable(idx, "myarrayvar", [6.7, 7.9])
-    myarrayvar = myobj.add_variable(idx, "myStronglytTypedVariable", ua.Variant([], ua.VariantType.UInt32))
-    myprop = myobj.add_property(idx, "myproperty", "I am a property")
-    mymethod = myobj.add_method(idx, "mymethod", func, [ua.VariantType.Int64], [ua.VariantType.Boolean])
-    multiply_node = myobj.add_method(idx, "multiply", multiply, [ua.VariantType.Int64, ua.VariantType.Int64], [ua.VariantType.Int64])
+    await mystringvar.set_writable()    # Set MyVariable to be writable by clients
+    mydtvar = await myobj.add_variable(idx, "MyDateTimeVar", datetime.utcnow())
+    await mydtvar.set_writable()    # Set MyVariable to be writable by clients
+    myarrayvar = await myobj.add_variable(idx, "myarrayvar", [6.7, 7.9])
+    myarrayvar = await myobj.add_variable(idx, "myStronglytTypedVariable", ua.Variant([], ua.VariantType.UInt32))
+    myprop = await myobj.add_property(idx, "myproperty", "I am a property")
+    mymethod = await myobj.add_method(idx, "mymethod", func, [ua.VariantType.Int64], [ua.VariantType.Boolean])
+    multiply_node = await myobj.add_method(idx, "multiply", multiply, [ua.VariantType.Int64, ua.VariantType.Int64], [ua.VariantType.Int64])
 
     # import some nodes from xml
-    server.import_xml("custom_nodes.xml")
+    await server.import_xml("custom_nodes.xml")
 
     # creating a default event object
     # The event object automatically will have members for all events properties
     # you probably want to create a custom event type, see other examples
-    myevgen = server.get_event_generator()
+    myevgen = await server.get_event_generator()
     myevgen.event.Severity = 300
 
     # starting!
-    server.start()
+    await server.start()
     print("Available loggers are: ", logging.Logger.manager.loggerDict.keys())
     try:
         # enable following if you want to subscribe to nodes on server side
@@ -119,17 +118,19 @@ async def main():
         #sub = server.create_subscription(500, handler)
         #handle = sub.subscribe_data_change(myvar)
         # trigger event, all subscribed clients wil receive it
-        var = myarrayvar.get_value()  # return a ref to value in db server side! not a copy!
+        var = await myarrayvar.get_value()  # return a ref to value in db server side! not a copy!
         var = copy.copy(var)  # WARNING: we need to copy before writting again otherwise no data change event will be generated
         var.append(9.3)
-        myarrayvar.set_value(var)
-        mydevice_var.set_value("Running")
+        await myarrayvar.set_value(var)
+        await mydevice_var.set_value("Running")
         myevgen.trigger(message="This is BaseEvent")
         server.set_attribute_value(myvar.nodeid, ua.DataValue(9.9))  # Server side write method which is a but faster than using set_value
+        while True:
+            await asyncio.sleep(1)
 
-        embed()
+
     finally:
-        server.stop()
+        await server.stop()
 
 
 if __name__ == "__main__":
