FreeOpcUa/python-opcua

Hope to fix an import of type_definitions, and parentlink.

opcua/common/xmlimporter.py
@@ -120,9 +120,9 @@ class XmlImporter(object):
             if ndata.parent:
                 ndata.parent = ua.NodeId.from_string(ndata.parent)
             if ndata.parentlink:
-                ndata.parentlink = self.to_nodeid(ndata.parentlink)
+                ndata.parentlink = self._to_nodeid(ndata.parentlink)
             if ndata.typedef:
-                ndata.typedef = self.to_nodeid(ndata.typedef)
+                ndata.typedef = self._to_nodeid(ndata.typedef)
             new_nodes.append(ndata)
         return new_nodes
 

FreeOpcUa/opcua-asyncio

asyncua/common/xmlimporter.py
@@ -115,9 +115,9 @@ class XmlImporter:
             if node_datum.parent:
                 node_datum.parent = ua.NodeId.from_string(node_datum.parent)
             if node_datum.parentlink:
-                node_datum.parentlink = self.to_nodeid(node_datum.parentlink)
+                node_datum.parentlink = self._to_nodeid(node_datum.parentlink)
             if node_datum.typedef:
-                node_datum.typedef = self.to_nodeid(node_datum.typedef)
+                node_datum.typedef = self._to_nodeid(node_datum.typedef)
             new_nodes.append(node_datum)
         return new_nodes
 