31db049abf600dbf577d15cdf05d75ba74baf2f7
Justin Teliet <jteliet@gmail.com>
2019-06-18 00:14:11 +0200

    fix server datavalur history example

--- examples/server-datavalue-history.py
+++ examples/server-datavalue-history.py
@@ -1,6 +1,6 @@
+import asyncio
 import sys
 sys.path.insert(0, "..")
-import time
 import math
 
 
@@ -8,40 +8,50 @@ from asyncua import ua, Server
 from asyncua.server.history_sql import HistorySQLite
 
 
-if __name__ == "__main__":
+async def main():
 
     # setup our server
     server = Server()
+    
+    # Configure server to use sqlite as history database (default is a simple memory dict)
+    server.iserver.history_manager.set_storage(HistorySQLite("my_datavalue_history.sql"))
+    
+    # initialize server 
+    await server.init()
+
     server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
 
     # setup our own namespace, not really necessary but should as spec
     uri = "http://examples.freeopcua.github.io"
-    idx = server.register_namespace(uri)
+    idx = await server.register_namespace(uri)
 
     # get Objects node, this is where we should put our custom stuff
     objects = server.get_objects_node()
 
     # populating our address space
-    myobj = objects.add_object(idx, "MyObject")
-    myvar = myobj.add_variable(idx, "MyVariable", ua.Variant(0, ua.VariantType.Double))
-    myvar.set_writable()  # Set MyVariable to be writable by clients
-
-    # Configure server to use sqlite as history database (default is a simple memory dict)
-    server.iserver.history_manager.set_storage(HistorySQLite("my_datavalue_history.sql"))
+    myobj = await objects.add_object(idx, "MyObject")
+    myvar = await myobj.add_variable(idx, "MyVariable", ua.Variant(0, ua.VariantType.Double))
+    await myvar.set_writable()  # Set MyVariable to be writable by clients
+    print(myvar)
 
     # starting!
-    server.start()
+    await server.start()
 
     # enable data change history for this particular node, must be called after start since it uses subscription
-    server.historize_node_data_change(myvar, period=None, count=100)
+    await server.historize_node_data_change(myvar, period=None, count=100)
 
     try:
         count = 0
         while True:
-            time.sleep(1)
+            await asyncio.sleep(1)
             count += 0.1
-            myvar.set_value(math.sin(count))
+            await myvar.set_value(math.sin(count))
 
     finally:
         # close connection, remove subscriptions, etc
-        server.stop()
+        await server.stop()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    loop.run_until_complete(main())
