4eb4cdbbb0e7d8168bdfc24291b75844646bdf62
mar-ar <marco.arnold@hermle.de>
2019-11-20 15:20:35 +0100

    added some examples

---
+++ examples/client-alarms-and-conditions.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+import asyncio
+
+from asyncua import Client, ua
+
+
+class SubHandler(object):
+
+    def datachange_notification(self, node, val, data):
+        print("Python: New data change event", node, val, data)
+
+    def event_notification(self, event):
+        print("Python new event:", event)
+
+
+class OpcUaClient(object):
+
+    def __init__(self, endpoint):
+        self.client = Client(endpoint)
+
+    async def init(self, ):
+        await self.client.connect()
+        objects = self.client.get_objects_node()
+        idx = await self.client.get_namespace_index("http://examples.freeopcua.github.io")
+
+        path = ['%s:ConditionObject' % idx]
+        con_obj = await objects.get_child(path)
+        condition = self.client.get_node(ua.NodeId(2830))
+
+        handler = SubHandler()
+        sub = await self.client.create_subscription(500, handler)
+        con_handle = await sub.subscribe_events(con_obj, condition)
+
+        path = ['%s:AlarmObject' % idx]
+        alarm_obj = await objects.get_child(path)
+        alarm = self.client.get_node(ua.NodeId(10637))
+
+        alarm_handle = await sub.subscribe_events(alarm_obj, alarm)
+
+
+async def start():
+    client = OpcUaClient("opc.tcp://localhost:4840")
+    await client.init()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(start())
+    loop.run_forever()
---
+++ examples/server-alarms-and-conditions.py
@@ -0,0 +1,107 @@
+# coding: utf-8
+
+from aioconsole import ainput
+
+import asyncio
+
+from asyncua import Server, ua
+
+
+class OpcUaServer(object):
+
+    def __init__(self, endpoint):
+        self.server = Server()
+        self.server.set_endpoint(endpoint)
+        self.server.set_server_name('Alarms and Conditions Test Server')
+        self.server.application_type = ua.ApplicationType.Server
+        self.con_gen = None
+        self.alarm_gen = None
+        self.alarm_gen2 = None
+
+    async def init(self, shelf_file=None):
+        await self.server.iserver.init(shelf_file)
+        uri = "http://examples.freeopcua.github.io"
+        idx = await self.server.register_namespace(uri)
+        objects = self.server.get_objects_node()
+
+        con_obj = await objects.add_object(idx, "ConditionObject")
+        condition = self.server.get_node(ua.NodeId(2830))
+        self.con_gen = await self.server.get_event_generator(condition, con_obj)
+        self.con_gen.event.add_property('NodeId', con_obj.nodeid, ua.VariantType.NodeId)
+
+        alarm_obj = await objects.add_object(idx, "AlarmObject")
+        alarm = self.server.get_node(ua.NodeId(10637))
+        self.alarm_gen = await self.server.get_event_generator(alarm, alarm_obj)
+        self.alarm_gen2 = await self.server.get_event_generator(alarm, alarm_obj)
+
+    def generate_condition(self, retain):
+        self.con_gen.event.ConditionName = 'Example Condition'
+        self.con_gen.event.Message = ua.LocalizedText("Some Message")
+        self.con_gen.event.Severity = 500
+        self.con_gen.event.BranchId = ua.NodeId(0)
+        if retain == 1:
+            self.con_gen.event.Retain = True
+        else:
+            self.con_gen.event.Retain = False
+        self.con_gen.trigger()
+
+    def generate_alarm(self, active):
+        self.alarm_gen.event.ConditionName = 'Example Alarm'
+        self.alarm_gen.event.Message = ua.LocalizedText("Some Message")
+        self.alarm_gen.event.Severity = 500
+        self.alarm_gen.event.BranchId = ua.NodeId(0)
+        self.alarm_gen.event.AckedState = ua.LocalizedText('Unacknowledged', 'en')
+        setattr(self.alarm_gen.event, 'AckedState/Id', False)
+        if active == 1:
+            self.alarm_gen.event.Retain = True
+            self.alarm_gen.event.ActiveState = ua.LocalizedText('Active', 'en')
+            setattr(self.alarm_gen.event, 'ActiveState/Id', True)
+        else:
+            self.alarm_gen.event.Retain = False
+            self.alarm_gen.event.ActiveState = ua.LocalizedText('Inactive', 'en')
+            setattr(self.alarm_gen.event, 'ActiveState/Id', False)
+        self.alarm_gen.trigger()
+
+    def generate_alarm2(self, active):
+        self.alarm_gen2.event.SourceName = 'andere source'
+        self.alarm_gen2.event.ConditionName = 'Example Alarm'
+        self.alarm_gen2.event.Message = ua.LocalizedText("Some Message")
+        self.alarm_gen2.event.Severity = 500
+        self.alarm_gen2.event.BranchId = ua.NodeId(0)
+        self.alarm_gen2.event.AckedState = ua.LocalizedText('Unacknowledged', 'en')
+        setattr(self.alarm_gen2.event, 'AckedState/Id', False)
+        if active == 1:
+            self.alarm_gen2.event.Retain = True
+            self.alarm_gen2.event.ActiveState = ua.LocalizedText('Active', 'en')
+            setattr(self.alarm_gen2.event, 'ActiveState/Id', True)
+        else:
+            self.alarm_gen2.event.Retain = False
+            self.alarm_gen2.event.ActiveState = ua.LocalizedText('Inactive', 'en')
+            setattr(self.alarm_gen2.event, 'ActiveState/Id', False)
+        self.alarm_gen2.trigger()
+
+
+async def interactive(server):
+    while True:
+        # server.generate_condition(1)
+        # server.generate_alarm(1)
+        line = await ainput(">>> ")
+        print('execute:', line)
+        try:
+            eval(line)
+        except Exception as msg:
+            print('Exception:', msg)
+            raise Exception
+
+
+async def main():
+    server = OpcUaServer("opc.tcp://0.0.0.0:4840")
+    await server.init()
+    await server.server.start()
+    await interactive(server)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
