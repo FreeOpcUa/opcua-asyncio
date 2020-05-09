aba1b182ed68cf738be09a34d01f44b826e0562b
oroulet <olivier@loke>
2020-05-09 09:51:58 +0200

    port server-enum.py to asyncua

--- examples/server-enum.py
+++ examples/server-enum.py
@@ -5,94 +5,67 @@
 '''
 import sys
 sys.path.insert(0, "..")
+import time
+import asyncio
 
-try:
-    from IPython import embed
-except ImportError:
-    import code
-
-    def embed():
-        vars = globals()
-        vars.update(locals())
-        shell = code.InteractiveConsole(vars)
-        shell.interact()
-
-interactive = True
-
+from IPython import embed
 
 from asyncua import ua, Server
-from asyncua.common import node
-from enum import IntEnum
 
-# Not required just for convenience
-# Because this example is based on EnumStrings, the values should start at 0 and no gaps are allowed.
-class MyEnum(IntEnum):
-    ok = 0
-    idle = 1
 
-# helper method to automatically create string list
-def enum_to_stringlist(a_enum):
-    items = []
-    for value in a_enum:
-        items.append(ua.LocalizedText(value.name))
-    return items
-
-if __name__ == "__main__":
+async def main():
     # setup our server
     server = Server()
+    await server.init()
     server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
 
     # setup our own namespace, not really necessary but should as spec
     uri = "http://examples.freeopcua.github.io"
-    nsidx = server.register_namespace(uri)
+    nsidx = await server.register_namespace(uri)
 
     # --------------------------------------------------------
     # create custom enum data type
     # --------------------------------------------------------
-    enums = server.get_root_node().get_child(["0:Types", "0:DataTypes", "0:BaseDataType", "0:Enumeration"])
 
     # 1.
     # Create Enum Type
-    myenum_type = enums.add_data_type(nsidx, 'MyEnum')
+    myenum_type = await server.nodes.enum_data_type.add_data_type(nsidx, 'MyEnum')
 
     # 2.
     # Add enumerations as EnumStrings (Not yet tested with EnumValues)
     # Essential to use namespace 0 for EnumStrings !
 
-    # By hand
-    #     es = myenum_type.add_variable(0, "EnumStrings" , [ua.LocalizedText("ok"),
-    #                                                       ua.LocalizedText("idle")])
+    es = await myenum_type.add_variable(0, "EnumStrings", [
+        ua.LocalizedText("ok"),
+        ua.LocalizedText("idle"),
+    ])
 
-    # Or convert the existing IntEnum MyEnum
-    es = myenum_type.add_property(0, "EnumStrings" , enum_to_stringlist(MyEnum))
+    # 3. load enums froms erver
+    await server.load_enums()
 
-    es.write_value_rank(1)
-    es.write_array_dimensions([0])
+    # now we have a python enum available to play with
+    val = ua.MyEnum.ok
+
+    # not sure these are necessary
+    #es.write_value_rank(1)
+    #es.write_array_dimensions([0])
 
     # --------------------------------------------------------
     # create object with enum variable
     # --------------------------------------------------------
-    # get Objects node, this is where we should put our custom stuff
-    objects = server.get_objects_node()
 
     # create object
-    myobj = objects.add_object(nsidx, 'MyObjectWithEnumVar')
+    myobj = await server.nodes.objects.add_object(nsidx, 'MyObjectWithEnumVar')
 
     # add var with as type the custom enumeration
-    myenum_var = myobj.add_variable(nsidx, 'MyEnum2Var', MyEnum.ok, datatype = myenum_type.nodeid)
-    myenum_var.set_writable()
-    myenum_var.write_value(MyEnum.idle)  # change value of enumeration
+    myenum_var = await myobj.add_variable(nsidx, 'MyEnum2Var', val, datatype=myenum_type.nodeid)
+    await myenum_var.set_writable()
+    await myenum_var.write_value(ua.MyEnum.idle)  # change value of enumeration
 
-    server.start()
-    try:
-        if interactive:
-            embed()
-        else:
-            while True:
-                time.sleep(0.5)
+    async with server:
+        while True:
+            time.sleep(0.5)
 
-    except IOError:
-        pass
-    finally:
-        server.stop()
-        print("done")
+
+if __name__ == "__main__":
+    asyncio.run(main())
