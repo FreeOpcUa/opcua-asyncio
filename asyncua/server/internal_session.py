4669915a0d8963d39d638a7cf0a20c4246aeaad0
Joey Faulkner <joeymfaulkner@gmail.com>
2020-05-25 14:51:22 +0200

    remove ridiculous change to generated code

--- asyncua/server/internal_session.py
+++ asyncua/server/internal_session.py
@@ -34,6 +34,7 @@ class InternalSession:
         self.subscription_service: SubscriptionService = submgr
         self.name = name
         self.user = user
+        print(self.user)
         self.nonce = None
         self.state = SessionState.Created
         self.session_id = ua.NodeId(self._counter)
