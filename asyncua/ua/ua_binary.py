ae61ca3bef344137da492a1f148136eb8f40279e
Andreas Heine <56362817+AndreasHeine@users.noreply.github.com>
2020-06-10 18:38:55 +0200

    Update ua_binary.py

--- asyncua/ua/ua_binary.py
+++ asyncua/ua/ua_binary.py
@@ -128,8 +128,11 @@ class _Primitive1(object):
     def pack_array(self, data):
         if data is None:
             return Primitives.Int32.pack(-1)
-        size_data = Primitives.Int32.pack(len(data))
-        return size_data + struct.pack(self._fmt.format(len(data)), *data)
+        if isinstance(data, list):
+                sizedata = Primitives.Int32.pack(len(data))
+                return sizedata + struct.pack(self._fmt.format(len(data)), *data)
+            else:
+                return Primitives.Int32.pack(-1)
 
     def unpack_array(self, data, length):
         if length == -1:
