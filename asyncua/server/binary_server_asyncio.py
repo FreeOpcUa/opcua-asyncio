3e0fce7629b8d072aecb1dbc9c40707559f7a3a6
Joey Faulkner <joeymfaulkner@gmail.com>
2020-08-24 12:17:43 +0200

    implement oroulet changes

--- asyncua/server/binary_server_asyncio.py
+++ asyncua/server/binary_server_asyncio.py
@@ -18,7 +18,7 @@ class OPCUAProtocol(asyncio.Protocol):
     Instantiated for every connection.
     """
 
-    def __init__(self, iserver: InternalServer, policies, clients, protocol_tasks):
+    def __init__(self, iserver: InternalServer, policies, clients, closing_tasks):
         self.peer_name = None
         self.transport = None
         self.processor = None
@@ -26,7 +26,7 @@ class OPCUAProtocol(asyncio.Protocol):
         self.iserver: InternalServer = iserver
         self.policies = policies
         self.clients = clients
-        self.protocol_tasks = protocol_tasks
+        self.closing_tasks = closing_tasks
         self.messages = asyncio.Queue()
         self._task = None
 
@@ -50,12 +50,11 @@ class OPCUAProtocol(asyncio.Protocol):
         self.transport.close()
         self.iserver.asyncio_transports.remove(self.transport)
         closing_task = self.iserver.loop.create_task(self.processor.close())
-        self.protocol_tasks.append(closing_task)
+        self.closing_tasks.append(closing_task)
         if self in self.clients:
             self.clients.remove(self)
         self.messages.put_nowait((None, None))
         self._task.cancel()
-        self.protocol_tasks.append(self._task)
 
     def data_received(self, data):
         self._buffer += data
@@ -111,7 +110,7 @@ class BinaryServer:
         self._server: Optional[asyncio.AbstractServer] = None
         self._policies = []
         self.clients = []
-        self.protocol_tasks = []
+        self.closing_tasks = []
 
     def set_policies(self, policies):
         self._policies = policies
@@ -122,7 +121,7 @@ class BinaryServer:
             iserver=self.iserver,
             policies=self._policies,
             clients=self.clients,
-            protocol_tasks=self.protocol_tasks,
+            closing_tasks=self.closing_tasks,
         )
 
     async def start(self):
@@ -136,6 +135,7 @@ class BinaryServer:
             self.hostname = sockname[0]
             self.port = sockname[1]
         self.logger.info('Listening on %s:%s', self.hostname, self.port)
+        await self._await_closing_tasks()
 
     async def stop(self):
         self.logger.info('Closing asyncio socket server')
@@ -143,12 +143,21 @@ class BinaryServer:
             transport.close()
 
         # Wait for all transport closing tasks to complete
-        results = await asyncio.gather(*self.protocol_tasks, return_exceptions=True)
+        results = await asyncio.gather(*self.closing_tasks, return_exceptions=True)
         for result in results:
             if isinstance(result, Exception):
                 self.logger.error(f"An error ocurred while closing a transport: {result}")
-        self.protocol_tasks = []
+        self.closing_tasks = []
 
         if self._server:
             self.iserver.loop.call_soon(self._server.close)
             await self._server.wait_closed()
+
+    async def _await_closing_tasks(self):
+        await asyncio.sleep(10)
+        while self.closing_tasks:
+            task = self.closing_tasks.pop()
+            try:
+                await task
+            except Exception:
+                logger.exception("Unexpected crash in BinaryServer._await_closing_tasks")
