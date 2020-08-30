b0ce213a7904be66925807b106349a330969fced
oroulet <olivier@loke>
2020-08-30 09:47:09 +0200

    do not catch CancelleError in closing_Task

--- asyncua/server/binary_server_asyncio.py
+++ asyncua/server/binary_server_asyncio.py
@@ -160,6 +160,9 @@ class BinaryServer:
             task = self.closing_tasks.pop()
             try:
                 await task
+            except asyncio.CancelledError:
+                # this means a stop request has been sent, it should not be catched
+                raise
             except Exception:
                 logger.exception("Unexpected crash in BinaryServer._await_closing_tasks")
         if recursive:
