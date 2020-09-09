6245c55270c2411ee987e0830664c99d6c44d49e
Julien Prigent <julienprigent@wanadoo.fr>
2020-09-09 13:58:56 -0700

    [ Cli tools ] Fix uaserver and tests
    This commits adds a server fixture running in a dedicated thread to handle OPC-UA
    requests properly (the existing server fixture accept TCP connections
    but are stuck at hello).
    
    We then use this fixture for the CLI tests.
    
    Also fixing the uaserver cli, its event_loop was stuck in a not async
    sleep preventing it from handling requests.

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,26 +1,53 @@
 import pytest
-from threading import Thread
-import time
 from unittest.mock import patch
 import sys
 import subprocess
+import concurrent.futures
 
-from asyncua.tools import uaread, uals, uawrite, uasubscribe, uahistoryread, uaserver, uaclient, uadiscover, uacall, uageneratestructs
+from asyncua.tools import uaread, uals, uawrite, uahistoryread, uaclient, uadiscover, uacall
 
-
-@pytest.mark.parametrize("tool", [uaread, uals, uawrite, uasubscribe, uahistoryread, uaclient, uadiscover, uacall, uageneratestructs])
-def test_that_tool_can_be_invoked_without_internal_error(tool):
-    # It's necessary to mock argv, else the tool is invoked with *pytest's* argv
-    with patch.object(sys, 'argv', [""]):
-        try:
-            tool()
-        except SystemExit:
-            pass
+pytestmark = pytest.mark.asyncio
 
 
-def test_that_server_can_be_invoked_without_internal_error():
-    proc = subprocess.Popen(['uaserver'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    time.sleep(2)
-    proc.send_signal(subprocess.signal.SIGINT)
-    proc.wait(timeout=2)
-    assert proc.returncode == 0
+async def test_cli_tools(running_server):
+    # admin privileges are only needed for uawrite
+    url = running_server.replace("//", "//admin@")
+    default_opts = ["mock_func", "-u", f"{url}"]
+    rw_node = ["-n", "i=3078"]
+    call_node = ["-n", "i=85"]
+    write_val = ["val_to_write"]
+    rw_opts = default_opts + rw_node
+    write_opts = rw_opts + write_val
+    call_opts = default_opts + call_node + write_val
+
+    tool_opts = {}
+    tool_opts[uaread] = rw_opts
+    tool_opts[uals] = rw_opts
+    tool_opts[uawrite] = write_opts
+    tool_opts[uahistoryread] = rw_opts
+    tool_opts[uaclient] = default_opts
+    tool_opts[uadiscover] = default_opts
+    tool_opts[uacall] = call_opts
+
+    for tool in (uaread, uals, uawrite, uahistoryread, uaclient, uadiscover, uacall):
+        # It's necessary to mock argv, else the tool is invoked with *pytest's* argv
+        with patch.object(sys, 'argv', tool_opts[tool]):
+            with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+                result = executor.submit(tool)
+                for future in concurrent.futures.as_completed([result]):
+                    exception = future.exception()
+                    assert (repr(exception) == "SystemExit(0)")
+
+
+async def test_cli_tools_which_require_sigint(running_server):
+    url = running_server
+    tools = (
+        ["tools/uaserver"], 
+        ["tools/uasubscribe", "-u", url, "-n", "i=3078"]
+    )
+    for tool in tools:
+        proc = subprocess.Popen(tool, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        with pytest.raises(subprocess.TimeoutExpired):
+            # we consider there's no error if the process is still alive
+            proc.communicate(timeout=2)
+        proc.send_signal(subprocess.signal.SIGINT)
