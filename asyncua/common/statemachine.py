93f0b1755b2a629187070e37c6dee4f918998418
Andreas Heine <56362817+AndreasHeine@users.noreply.github.com>
2020-11-26 14:11:40 +0100

    Update statemachine.py

--- asyncua/common/statemachine.py
+++ asyncua/common/statemachine.py
@@ -1,1 +1,35 @@
-# WIP
+import asyncio
+
+from asyncua import Server, ua, Node
+from asyncua.common.instantiate_util import instantiate
+
+class StateMachineTypeClass(object):
+    def __init__(self, server=None, parent=None):
+        self._server = server
+        self._parent = parent
+        raise NotImplementedError
+
+class FiniteStateMachineTypeClass(StateMachineTypeClass):
+    def __init__(self, server=None, parent=None):
+        super().__init__(server, parent)  
+        raise NotImplementedError
+
+class ExclusiveLimitStateMachineTypeClass(FiniteStateMachineTypeClass):
+    def __init__(self, server=None, parent=None):
+        super().__init__(server, parent)
+        raise NotImplementedError
+
+class FileTransferStateMachineTypeClass(FiniteStateMachineTypeClass):
+    def __init__(self, server=None, parent=None):
+        super().__init__(server, parent)
+        raise NotImplementedError
+
+class ProgramStateMachineTypeClass(FiniteStateMachineTypeClass):
+    def __init__(self, server=None, parent=None):
+        super().__init__(server, parent)
+        raise NotImplementedError
+
+class ShelvedStateMachineTypeClass(FiniteStateMachineTypeClass):
+    def __init__(self, server=None, parent=None):
+        super().__init__(server, parent)
+        raise NotImplementedError
