9d1edc7bcff45f832da245662a8c9eb8acf70e6f
Andreas Heine <56362817+AndreasHeine@users.noreply.github.com>
2020-11-27 09:11:46 +0100

    Update statemachine.py

--- asyncua/common/statemachine.py
+++ asyncua/common/statemachine.py
@@ -8,8 +8,8 @@ class StateMachineTypeClass(object):
     Implementation of an StateMachineType
     '''
     def __init__(self, server=None, parent=None, idx=None, name=None):
-        if not server: raise ValueError #change to check instance type
-        if not parent: raise ValueError #change to check instance type
+        if not server: raise ValueError #FIXME change to check instance type
+        if not parent: raise ValueError #FIXME change to check instance type
         if idx == None:
             idx = parent.nodeid.NamespaceIndex
         if name == None:
@@ -31,37 +31,46 @@ class StateMachineTypeClass(object):
         setup adressspace and initialize 
         '''
         self._optionals = optionals
-        self._state_machine_node = await self._parent.add_object(self._idx, self._name, objecttype=self._state_machine_type, instantiate_optional=optionals)
+        self._state_machine_node = await self._parent.add_object(
+            self._idx, 
+            self._name, 
+            objecttype=self._state_machine_type, 
+            instantiate_optional=optionals
+            )
         self._current_state_node = await self._state_machine_node.get_child(["CurrentState"])
         self._current_state_id_node = await self._state_machine_node.get_child(["CurrentState","Id"])
         if self._optionals:
             self._current_transition_node = await self._state_machine_node.get_child(["LastTransition"])
             self._current_transition_id_node = await self._state_machine_node.get_child(["LastTransition","Id"])
-        
-        #initialise values
-        #maybe its smart to check parents child for a initial state instance (InitialStateType) and initialize it with its id but if no state instance is brovided ...
-        
+
+        #FIXME initialise values
+        '''
+        maybe its smart to check parents child for a initial state instance (InitialStateType) 
+        and initialize it with its id but if no state instance is provided ... i will log a 
+        warning
+        '''
+
     async def change_state(self, state_name, state_node, transition_name=None, transition_node=None):
         '''
+        method to change the state of the statemachine
         state_name: ua.LocalizedText()
         state: ua.NodeId() <- StateType node
         transition_name: ua.LocalizedText()
         transition: ua.NodeId() <- TransitionType node
         '''
-        #check types/class
-        #check StateType exist
-        #check TransitionTypeType exist
+        #FIXME check StateType exist
+        #FIXME check TransitionTypeType exist
         await self.write_state(state_name, state_node)
         if self._optionals and transition_name and transition_node:
             await self.write_transition(transition_name, transition_node)
 
     async def write_state(self, state_name, state_node):
-        #check types/class
+        #FIXME check types/class
         await self._current_state_node.write_value(state_name)
         await self._current_state_id_node.write_value(state_node)
 
     async def write_transition(self, transition_name, transition_node):
-        #check types/class
+        #FIXME check types/class
         await self._last_transition_node.write_value(transition_name)
         await self._last_transition_id_node.write_value(transition_node)
 
@@ -82,7 +91,12 @@ class FiniteStateMachineTypeClass(StateMachineTypeClass):
         setup adressspace and initialize 
         '''
         self._optionals = optionals
-        self._state_machine_node = await self._parent.add_object(self._idx, self._name, objecttype=self._state_machine_type, instantiate_optional=optionals)
+        self._state_machine_node = await self._parent.add_object(
+            self._idx, 
+            self._name, 
+            objecttype=self._state_machine_type, 
+            instantiate_optional=optionals
+            )
 
     async def set_avalible_states(self, states):
         self._avalible_states = states
@@ -142,13 +156,32 @@ class ProgramStateMachineTypeClass(FiniteStateMachineTypeClass):
         self._start_method_node = None #uamethod node
         self._suspend_method_node = None #uamethod node
 
+        self.lt_ready = ua.LocalizedText("Ready", "en-US")
+        self.lt_running = ua.LocalizedText("Running", "en-US")
+        self.lt_halted = ua.LocalizedText("Halted", "en-US")
+        self.lt_suspended= ua.LocalizedText("Suspended", "en-US")
+        self.lt_halted_to_ready = ua.LocalizedText("HaltedToReady", "en-US")
+        self.lt_ready_to_running = ua.LocalizedText("ReadyToRunning", "en-US")
+        self.lt_running_to_halted = ua.LocalizedText("RunningToHalted", "en-US")
+        self.lt_running_to_ready = ua.LocalizedText("RunningToReady", "en-US")
+        self.lt_running_to_suspended = ua.LocalizedText("RunningToSuspended", "en-US")
+        self.lt_suspended_to_running = ua.LocalizedText("SuspendedToRunning", "en-US")
+        self.lt_suspended_to_halted = ua.LocalizedText("SuspendedToHalted", "en-US")
+        self.lt_suspended_to_ready = ua.LocalizedText("SuspendedToReady", "en-US")
+        self.lt_ready_to_halted = ua.LocalizedText("ReadyToHalted", "en-US")
+
     async def install(self, optionals=False):
         '''
         setup adressspace and initialize 
         '''
         self._optionals = optionals
-        self._state_machine_node = await self._parent.add_object(self._idx, self._name, objecttype=self._state_machine_type, instantiate_optional=optionals)
-        #get childnodes:
+        self._state_machine_node = await self._parent.add_object(
+            self._idx, 
+            self._name, 
+            objecttype=self._state_machine_type, 
+            instantiate_optional=optionals
+            )
+        #FIXME get childnodes:
         self._ready_state = None #State node
         self._halted_state = None #State node
         self._running_state = None #State node
@@ -170,111 +203,228 @@ class ProgramStateMachineTypeClass(FiniteStateMachineTypeClass):
 
     #Transition
     async def HaltedToReady(self):
-        await self._current_state.write_value(ua.LocalizedText("Ready", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._current_state_id.write_value(self._ready_state.nodeid, varianttype=ua.VariantType.NodeId)
-        await self._last_transition.write_value(ua.LocalizedText("HaltedToReady", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._last_transition_id.write_value(self._halted_to_ready.nodeid, varianttype=ua.VariantType.NodeId)
+        await self._current_state.write_value(
+            self.lt_ready,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._current_state_id.write_value(
+            self._ready_state.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        await self._last_transition.write_value(
+            self.lt_halted_to_ready,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._last_transition_id.write_value(
+            self._halted_to_ready.nodeid,
+            varianttype=ua.VariantType.NodeId
+            )
+        #FIXME trigger event
         return ua.StatusCode(ua.status_codes.StatusCodes.Good)
 
     #Transition
     async def ReadyToRunning(self):
-        await self._current_state.write_value(ua.LocalizedText("Running", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._current_state_id.write_value(self._running_state.nodeid, varianttype=ua.VariantType.NodeId)
-        await self._last_transition.write_value(ua.LocalizedText("ReadyToRunning", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._last_transition_id.write_value(self._ready_to_running.nodeid, varianttype=ua.VariantType.NodeId)
+        await self._current_state.write_value(
+            self.lt_running,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._current_state_id.write_value(
+            self._running_state.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        await self._last_transition.write_value(
+            self.lt_ready_to_running,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._last_transition_id.write_value(
+            self._ready_to_running.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        #FIXME trigger event
         return ua.StatusCode(ua.status_codes.StatusCodes.Good)
 
     #Transition
     async def RunningToHalted(self):
-        await self._current_state.write_value(ua.LocalizedText("Halted", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._current_state_id.write_value(self._halted_state.nodeid, varianttype=ua.VariantType.NodeId)
-        await self._last_transition.write_value(ua.LocalizedText("RunningToHalted", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._last_transition_id.write_value(self._running_to_halted.nodeid, varianttype=ua.VariantType.NodeId)
+        await self._current_state.write_value(
+            self.lt_halted,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._current_state_id.write_value(
+            self._halted_state.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        await self._last_transition.write_value(
+            self.lt_running_to_halted,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._last_transition_id.write_value(
+            self._running_to_halted.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        #FIXME trigger event
         return ua.StatusCode(ua.status_codes.StatusCodes.Good)
 
     #Transition
     async def RunningToReady(self):
-        await self._current_state.write_value(ua.LocalizedText("Ready", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._current_state_id.write_value(self._ready_state.nodeid, varianttype=ua.VariantType.NodeId)
-        await self._last_transition.write_value(ua.LocalizedText("RunningToReady", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._last_transition_id.write_value(self._running_to_ready.nodeid, varianttype=ua.VariantType.NodeId)
+        await self._current_state.write_value(
+            self.lt_ready,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._current_state_id.write_value(
+            self._ready_state.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        await self._last_transition.write_value(
+            self.lt_running_to_ready,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._last_transition_id.write_value(
+            self._running_to_ready.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        #FIXME trigger event
         return ua.StatusCode(ua.status_codes.StatusCodes.Good)
 
     #Transition
     async def RunningToSuspended(self):
-        await self._current_state.write_value(ua.LocalizedText("Suspended", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._current_state_id.write_value(self._suspended_state.nodeid, varianttype=ua.VariantType.NodeId)
-        await self._last_transition.write_value(ua.LocalizedText("RunningToSuspended", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._last_transition_id.write_value(self._running_to_suspended.nodeid, varianttype=ua.VariantType.NodeId)
+        await self._current_state.write_value(
+            self.lt_suspended,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._current_state_id.write_value(
+            self._suspended_state.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        await self._last_transition.write_value(
+            self.lt_running_to_suspended,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._last_transition_id.write_value(
+            self._running_to_suspended.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        #FIXME trigger event
         return ua.StatusCode(ua.status_codes.StatusCodes.Good)
 
     #Transition 
     async def SuspendedToRunning(self):
-        await self._current_state.write_value(ua.LocalizedText("Running", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._current_state_id.write_value(self._running_state.nodeid, varianttype=ua.VariantType.NodeId)
-        await self._last_transition.write_value(ua.LocalizedText("SuspendedToRunning", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._last_transition_id.write_value(self._suspended_to_running.nodeid, varianttype=ua.VariantType.NodeId)
+        await self._current_state.write_value(
+            self.lt_running,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._current_state_id.write_value(
+            self._running_state.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        await self._last_transition.write_value(
+            self.lt_suspended_to_running,
+            varianttype=ua.VariantType.LocalizedText
+            )
+        await self._last_transition_id.write_value(
+            self._suspended_to_running.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        #FIXME trigger event
         return ua.StatusCode(ua.status_codes.StatusCodes.Good)
 
     #Transition
     async def SuspendedToHalted(self):
-        await self._current_state.write_value(ua.LocalizedText("Halted", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._current_state_id.write_value(self._halted_state.nodeid, varianttype=ua.VariantType.NodeId)
-        await self._last_transition.write_value(ua.LocalizedText("SuspendedToHalted", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._last_transition_id.write_value(self._suspended_to_halted.nodeid, varianttype=ua.VariantType.NodeId)
+        await self._current_state.write_value(
+            self.lt_halted,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._current_state_id.write_value(
+            self._halted_state.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        await self._last_transition.write_value(
+            self.lt_suspended_to_halted,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._last_transition_id.write_value(
+            self._suspended_to_halted.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        #FIXME trigger event
         return ua.StatusCode(ua.status_codes.StatusCodes.Good)
 
     #Transition
     async def SuspendedToReady(self):
-        await self._current_state.write_value(ua.LocalizedText("Ready", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._current_state_id.write_value(self._ready_state.nodeid, varianttype=ua.VariantType.NodeId)
-        await self._last_transition.write_value(ua.LocalizedText("SuspendedToReady", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._last_transition_id.write_value(self._suspended_to_ready.nodeid, varianttype=ua.VariantType.NodeId)
+        await self._current_state.write_value(
+            self.lt_ready,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._current_state_id.write_value(
+            self._ready_state.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        await self._last_transition.write_value(
+            self.lt_suspended_to_ready,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._last_transition_id.write_value(
+            self._suspended_to_ready.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        #FIXME trigger event
         return ua.StatusCode(ua.status_codes.StatusCodes.Good)
 
     #Transition 
     async def ReadyToHalted(self):
-        await self._current_state.write_value(ua.LocalizedText("Halted", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._current_state_id.write_value(self._halted_state.nodeid, varianttype=ua.VariantType.NodeId)
-        await self._last_transition.write_value(ua.LocalizedText("ReadyToHalted", "en-US"), varianttype=ua.VariantType.LocalizedText)
-        await self._last_transition_id.write_value(self._ready_to_halted.nodeid, varianttype=ua.VariantType.NodeId)
+        await self._current_state.write_value(
+            self.lt_halted,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._current_state_id.write_value(
+            self._halted_state.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        await self._last_transition.write_value(
+            self.lt_ready_to_halted,
+            varianttype=ua.VariantType.LocalizedText
+            ) 
+        await self._last_transition_id.write_value(
+            self._ready_to_halted.nodeid, 
+            varianttype=ua.VariantType.NodeId
+            )
+        #FIXME trigger event
         return ua.StatusCode(ua.status_codes.StatusCodes.Good)
 
     #method to be linked to uamethod
     async def Start(self):
-        if await self._current_state.read_value() == ua.LocalizedText("Ready", "en-US"):
+        if await self._current_state.read_value() == self.lt_ready:
             return await ReadyToRunning()
         else:
             return ua.StatusCode(ua.status_codes.StatusCodes.BadNotExecutable)
 
     #method to be linked to uamethod
     async def Suspend(self):
-        if await self._current_state.read_value() == ua.LocalizedText("Running", "en-US"):
+        if await self._current_state.read_value() == self.lt_running:
             return await RunningToSuspended()
         else:
             return ua.StatusCode(ua.status_codes.StatusCodes.BadNotExecutable)
 
     #method to be linked to uamethod
     async def Resume(self):
-        if await self._current_state.read_value() == ua.LocalizedText("Suspended", "en-US"):
+        if await self._current_state.read_value() == self.lt_suspended:
             return await SuspendedToRunning()
         else:
             return ua.StatusCode(ua.status_codes.StatusCodes.BadNotExecutable)
 
     #method to be linked to uamethod
     async def Halt(self):
-        if await self._current_state.read_value() == ua.LocalizedText("Ready", "en-US"):
+        if await self._current_state.read_value() == self.lt_ready:
             return await ReadyToHalted()
-        elif await self._current_state.read_value() == ua.LocalizedText("Running", "en-US"):
+        elif await self._current_state.read_value() == self.lt_running:
             return await RunningToHalted()
-        elif await self._current_state.read_value() == ua.LocalizedText("Suspended", "en-US"):
+        elif await self._current_state.read_value() == self.lt_suspended:
             return await SuspendedToHalted()
         else:
             return ua.StatusCode(ua.status_codes.StatusCodes.BadNotExecutable)
 
     #method to be linked to uamethod
     async def Reset(self):
-        if await self._current_state.read_value() == ua.LocalizedText("Halted", "en-US"):
+        if await self._current_state.read_value() == self.lt_halted:
             return await HaltedToReady()
         else:
             return ua.StatusCode(ua.status_codes.StatusCodes.BadNotExecutable)
