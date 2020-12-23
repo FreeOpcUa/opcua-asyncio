693eb30644260755494e8dbccee3c8074938c3d3
Adam <adam@nimbuscontrols.com>
2020-12-23 09:35:05 -0500

    update read file example

--- examples/client-file.py
+++ examples/client-file.py
@@ -2,17 +2,12 @@ import asyncio
 from asyncua import Client
 
 async def read_file():
-    """ read file examples """
+    """ read file example """
 
     url = "opc.tcp://10.0.0.199:4840"
     async with Client(url=url) as client:
-        # option 1
-        contents = await client.read_file(index=2, name_of_node="NameOfNode")
-        print(contents)
-
-        # option 2
         node = client.get_node("ns=2;s=NameOfNode")
-        contents = await client.read_file(node=node)
+        contents = await client.read_file(node)
         print(contents)
 
 asyncio.run(read_file())
