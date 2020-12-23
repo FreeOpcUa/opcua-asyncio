c533614b40c58c481c4cfcfda07e4b3edc9b3d24
Adam <adam@nimbuscontrols.com>
2020-12-23 12:44:40 -0500

    update ua file example with new class

--- examples/client-file.py
+++ examples/client-file.py
@@ -1,13 +1,15 @@
 import asyncio
-from asyncua import Client
+from asyncua.client import Client
+from asyncua.client.ua_file import UaFile
 
 async def read_file():
     """ read file example """
 
     url = "opc.tcp://10.0.0.199:4840"
     async with Client(url=url) as client:
-        node = client.get_node("ns=2;s=NameOfNode")
-        contents = await client.read_file(node)
+        file_node = client.get_node("ns=2;s=NameOfNode")
+        ua_file_client = UaFile(file_node)
+        contents = await ua_file_client.read_file()
         print(contents)
 
 asyncio.run(read_file())
