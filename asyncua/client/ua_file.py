943caededce99fbaee7c86260069c52c977e2ee7
Adam <adam@nimbuscontrols.com>
2020-12-24 10:54:03 -0500

    separate file read class

--- asyncua/client/ua_file.py
+++ asyncua/client/ua_file.py
@@ -5,12 +5,7 @@ class UaFile:
 
     def __init__(self, file_node):
         self._file_node = file_node
-
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, exc_type, exc_value, traceback):
-        return exc_type is None
+        self._handle = None
 
     async def open(self, open_mode):
         """ open file method """
@@ -18,31 +13,34 @@ class UaFile:
         arg = ua.Variant(open_mode, ua.VariantType.Byte)
         return await self._file_node.call_method(open_node, arg)
 
+    async def close(self):
+        """ close file method """
+        read_node = await self._file_node.get_child("Close")
+        arg1 = ua.Variant(self._handle, ua.VariantType.UInt32)
+        return await self._file_node.call_method(read_node, arg1)
+
+
+class UaFileRead(UaFile):
+    def __init__(self, file_node):
+        super().__init__(file_node)
+
+    async def __aenter__(self):
+        self._handle = await self.open(ua.OpenFileMode.Read.value)
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        return await self.close()
+
+    async def read(self):
+        """ reads file contents """
+        size = await self.get_size()
+        read_node = await self._file_node.get_child("Read")
+        arg1 = ua.Variant(self._handle, ua.VariantType.UInt32)
+        arg2 = ua.Variant(size, ua.VariantType.Int32)
+        return await self._file_node.call_method(read_node, arg1, arg2)
+
     async def get_size(self):
         """ gets size of file """
         size_node = await self._file_node.get_child("Size")
         return await size_node.read_value()
 
-    async def close(self, handle):
-        """ close file method """
-        read_node = await self._file_node.get_child("Close")
-        arg1 = ua.Variant(handle, ua.VariantType.UInt32)
-        return await self._file_node.call_method(read_node, arg1)
-
-    async def read(self, handle, size):
-        """
-        :param handle: handle from open()
-        :param size: size of file from from get_size()
-        """
-        read_node = await self._file_node.get_child("Read")
-        arg1 = ua.Variant(handle, ua.VariantType.UInt32)
-        arg2 = ua.Variant(size, ua.VariantType.Int32)
-        return await self._file_node.call_method(read_node, arg1, arg2)
-
-    async def read_once(self):
-        """ open, read, close in one operation """
-        handle = await self.open(ua.OpenFileMode.Read.value)
-        size = await self.get_size()
-        contents = await self.read(handle, size)
-        await self.close(handle)
-        return contents
--- examples/client-file.py
+++ examples/client-file.py
@@ -1,27 +1,16 @@
 import asyncio
-from asyncua import ua
 from asyncua.client import Client
-from asyncua.client.ua_file import UaFile
-
+from asyncua.client.ua_file import UaFileRead
 
 async def read_file():
     """ read file example """
 
     url = "opc.tcp://10.0.0.199:4840"
     async with Client(url=url) as client:
-
         file_node = client.get_node("ns=2;s=NameOfNode")
-        async with UaFile(file_node) as ua_file:
-            # open(), read(), close() all in one operation
-            contents = await ua_file.read_once()
+        async with UaFileRead(file_node) as ua_file:
+            # read file
+            contents = await ua_file.read()
             print(contents)
-
-            # handling all methods by the user
-            handle = await ua_file.open(ua.OpenFileMode.Read.value)
-            size = await ua_file.get_size()
-            contents = await ua_file.read(handle, size)
-            await ua_file.close(handle)
-            print(contents)
-
 
 asyncio.run(read_file())
