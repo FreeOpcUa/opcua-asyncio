72d171d7e763bc3442abf8d893e843f48aca35b6
oroulet <olivier@r-dt.net>
2021-01-03 14:59:58 +0100

    make fundamental struct 1.04 creation work

--- examples/server-data-type-definition.py
+++ examples/server-data-type-definition.py
@@ -18,15 +18,9 @@ async def main():
     # setup our own namespace, not really necessary but should as spec
     uri = 'http://examples.freeopcua.github.io'
     idx = await server.register_namespace(uri)
-    mystruct = await server.nodes.base_structure_type.add_data_type(idx, "MyStruct")
-    sdef = ua.StructureDefinition()
-    sdef.StructureType = ua.StructureType.Structure
-    sdef.Fields = [new_struct_field("MyBool", ua.VariantType.Boolean), new_struct_field("MyUInt32", ua.VariantType.UInt32)]
-    await mystruct.write_data_type_definition(sdef)
 
-    await new_struct(server, idx, "MySimpleStruct", [new_struct_field("MyBool", ua.VariantType.Boolean), new_struct_field("MyUInt32", ua.VariantType.UInt32)])
+    await new_struct(server, idx, "MyStruct", [new_struct_field("MyBool", ua.VariantType.Boolean), new_struct_field("MyUInt32", ua.VariantType.UInt32)])
     await new_enum(server, idx, "MyEnum", "titi", "toto", "tutu")
-
 
     async with server:
         while True:
