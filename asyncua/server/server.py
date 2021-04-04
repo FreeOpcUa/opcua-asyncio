f39f72578d2956f2f5c33c437897ea18ed5d3d87
Andreas Heine <56362817+AndreasHeine@users.noreply.github.com>
2021-04-04 20:24:58 +0200

    Update server.py

--- asyncua/server/server.py
+++ asyncua/server/server.py
@@ -670,8 +670,8 @@ class Server:
         """
         return await self.iserver.write_attribute_value(nodeid, datavalue, attr)
 
-    async def read_attribute_value(self, nodeid, attr=ua.AttributeIds.Value):
+    def read_attribute_value(self, nodeid, attr=ua.AttributeIds.Value):
         """
         directly read datavalue of the Attribute
         """
-        return await self.iserver.read_attribute_value(nodeid, attr)
+        return self.iserver.read_attribute_value(nodeid, attr)
