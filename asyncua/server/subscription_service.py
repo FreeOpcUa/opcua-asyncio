4b243a0bc4a7324e5be8f0a9cd569fc3c3a0fa2a
Christopher Bremner <chrisjbremner@gmail.com>
2021-11-05 10:23:26 -0700

    Avoid false warning when deleting subscription

--- asyncua/server/subscription_service.py
+++ asyncua/server/subscription_service.py
@@ -66,11 +66,12 @@ class SubscriptionService:
             else:
                 existing_subs.append(sub)
                 res.append(ua.StatusCode())
-        exceptions = await asyncio.gather(
+        stop_results = await asyncio.gather(
             *[sub.stop() for sub in existing_subs], return_exceptions=True
         )
-        for exc in exceptions:
-            self.logger.warning("Exception while stopping subscription", exc_info=exc)
+        for res in stop_results:
+            if isinstance(res, Exception):
+                self.logger.warning("Exception while stopping subscription", exc_info=res)
         return res
 
     def publish(self, acks: Iterable[ua.SubscriptionAcknowledgement]):
