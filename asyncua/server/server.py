0dff796d03a0d3da78ee54bf87dfc6cebfa1a927
Alexander Schrode <Midix01@googlemail.com>
2022-06-09 19:05:58 +0200

    XMLExport: make export values optional

--- asyncua/server/server.py
+++ asyncua/server/server.py
@@ -589,26 +589,28 @@ class Server:
         importer = XmlImporter(self)
         return await importer.import_xml(path, xmlstring)
 
-    async def export_xml(self, nodes, path):
+    async def export_xml(self, nodes, path, export_values: bool = False):
         """
         Export defined nodes to xml
+        :param export_value: export values from variants
         """
-        exp = XmlExporter(self)
+        exp = XmlExporter(self, export_values=export_values)
         await exp.build_etree(nodes)
         await exp.write_xml(path)
 
-    async def export_xml_by_ns(self, path: str, namespaces: list = None):
+    async def export_xml_by_ns(self, path: str, namespaces: list = None, export_values: bool = False):
         """
         Export nodes of one or more namespaces to an XML file.
         Namespaces used by nodes are always exported for consistency.
         :param path: name of the xml file to write
         :param namespaces: list of string uris or int indexes of the namespace to export,
+        :param export_values: export values from variants
          if not provide all ns are used except 0
         """
         if namespaces is None:
             namespaces = []
         nodes = await get_nodes_of_namespace(self, namespaces)
-        await self.export_xml(nodes, path)
+        await self.export_xml(nodes, path, export_values=export_values)
 
     async def delete_nodes(self, nodes, recursive=False) -> Coroutine:
         return await delete_nodes(self.iserver.isession, nodes, recursive)
