c1ffbfcf7c0f4b6e1b9d0a1b013bf310e2098a20
Alexander Schrode <Midix01@googlemail.com>
2022-08-31 18:00:09 +0200

    server handle malformed packets

--- tests/test_connections.py
+++ tests/test_connections.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 import pytest
-
-from asyncua import Client
+import asyncio
+from asyncua import Client, ua
 from asyncua.ua.uaerrors import BadMaxConnectionsReached
-
+import struct
 from .conftest import port_num
 
 pytestmark = pytest.mark.asyncio
@@ -23,6 +23,19 @@ async def test_max_connections_1(opc):
                 async with Client(f'opc.tcp://127.0.0.1:{port}'):
                     pass
     opc.server.iserver.isession.__class__.max_connections = 1000
+
+
+async def test_oom_server(opc):
+    port = opc.server.endpoint.port
+    async with Client(f'opc.tcp://127.0.0.1:{port}') as c:
+        # craft invalid packet that trigger oom
+        message_type, chunk_type, packet_size = [ua.MessageType.SecureOpen, b'E', 0]
+        c.uaclient.protocol.transport.write(struct.pack("<3scI", message_type, chunk_type, packet_size))
+        # sleep to give the server time to handle the message because we bypass the asyncio
+        await asyncio.sleep(1.0)
+        # now try to read a value to see if server is still alive
+        server_time_node = c.get_node(ua.NodeId(ua.ObjectIds.Server_ServerStatus_CurrentTime))
+        await server_time_node.read_value()
 
 
 async def test_safe_disconnect():
