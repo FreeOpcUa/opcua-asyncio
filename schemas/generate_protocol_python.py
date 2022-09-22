defa1ffaa63c13d3da98ddaa3ece4146a774e0d8
Alexander Schrode <Midix01@googlemail.com>
2022-09-22 19:58:16 +0200

    update schema to v1_05

--- schemas/generate_protocol_python.py
+++ schemas/generate_protocol_python.py
@@ -145,12 +145,12 @@ class CodeGenerator:
 
         for field in obj.fields:
             typestring = field.data_type
+            if field.allow_subtypes and typestring != 'ExtensionObject':
+                typestring = f"Type[{typestring}]"
             if field.is_array():
                 typestring = f"List[{typestring}]"
             if field.is_optional:
                 typestring = f"Optional[{typestring}]"
-            if field.allow_subtypes:
-                typestring = f"Type[{typestring}]"
             if field.name == field.data_type:
                 # variable name and type name are the same. Dataclass do not like it
                 hack_names.append(field.name)
