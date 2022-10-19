307673c626e4684ae678ace8c990e4f473d680ec
Andreas Heine <info@andreas-heine.net>
2022-10-19 14:54:27 +0200

    make abstract methods async

--- asyncua/common/session_interface.py
+++ asyncua/common/session_interface.py
@@ -7,61 +7,61 @@ from asyncua import ua
 
 class AbstractSession(ABC):
     @abstractmethod
-    def browse(self, parameters: ua.BrowseParameters) -> List[ua.BrowseResult]:
+    async def browse(self, parameters: ua.BrowseParameters) -> List[ua.BrowseResult]:
         '''
 
         '''
     
     @abstractmethod
-    def browse_next(self, parameters: ua.BrowseNextParameters) -> List[ua.BrowseResult]:
+    async def browse_next(self, parameters: ua.BrowseNextParameters) -> List[ua.BrowseResult]:
         '''
         
         '''
 
     @abstractmethod
-    def read(self, parameters: ua.ReadParameters) -> List[ua.DataValue]:
+    async def read(self, parameters: ua.ReadParameters) -> List[ua.DataValue]:
         '''
 
         '''
 
     @abstractmethod
-    def write(self, parameters: ua.WriteParameters) -> List[ua.StatusCode]:
+    async def write(self, parameters: ua.WriteParameters) -> List[ua.StatusCode]:
         '''
 
         '''
 
     @abstractmethod
-    def translate_browsepaths_to_nodeids(self, browse_paths: List[ua.BrowsePath]) -> List[ua.BrowsePathResult]:
+    async def translate_browsepaths_to_nodeids(self, browse_paths: List[ua.BrowsePath]) -> List[ua.BrowsePathResult]:
         '''
 
         '''
 
     @abstractmethod
-    def history_read(self, params: ua.HistoryReadParameters) -> ua.HistoryReadResult:
+    async def history_read(self, params: ua.HistoryReadParameters) -> ua.HistoryReadResult:
         '''
 
         '''
 
     @abstractmethod
-    def delete_references(self, refs: List[ua.DeleteReferencesItem]) -> List[ua.StatusCode]:
+    async def delete_references(self, refs: List[ua.DeleteReferencesItem]) -> List[ua.StatusCode]:
         '''
 
         '''
 
     @abstractmethod
-    def add_references(self, refs: List[ua.AddReferencesItem]) -> List[ua.StatusCode]:
+    async def add_references(self, refs: List[ua.AddReferencesItem]) -> List[ua.StatusCode]:
         '''
 
         '''
 
     @abstractmethod
-    def register_nodes(self, nodes: List[ua.NodeId]) -> List[ua.NodeId]:
+    async def register_nodes(self, nodes: List[ua.NodeId]) -> List[ua.NodeId]:
         '''
 
         '''
 
     @abstractmethod
-    def unregister_nodes(self, nodes: List[ua.NodeId]) -> List[ua.NodeId]:
+    async def unregister_nodes(self, nodes: List[ua.NodeId]) -> List[ua.NodeId]:
         '''
 
         '''
