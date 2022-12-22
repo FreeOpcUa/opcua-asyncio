5755400aec4dae3b0c0f7dcba1e0127bd4203277
Alexander Schrode <Midix01@googlemail.com>
2022-12-22 21:19:02 +0100

    remove magic numbers

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -546,7 +546,7 @@ def test_datetime():
     assert ua.datetime_to_win_epoch(ua.win_epoch_to_datetime(epch)) == epch
     # Test if values that are out of range are either min or max
     assert ua.datetime_to_win_epoch(datetime.min) == 0
-    assert ua.datetime_to_win_epoch(datetime.max) == (2**63) - 1
+    assert ua.datetime_to_win_epoch(datetime.max) == ua.MAX_INT64
 
 
 def test_equal_nodeid():
