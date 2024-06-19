474e914dd1b0ce940cf16c7b4da27f43ff5b2614
Chris Bremner <cbremner@ginkgobioworks.com>
2024-06-19 15:34:12 -0700

    Fix epoch to UTC

--- asyncua/ua/uatypes.py
+++ asyncua/ua/uatypes.py
@@ -225,10 +225,10 @@ def get_win_epoch():
 def win_epoch_to_datetime(epch):
     if epch >= MAX_OPC_FILETIME:
         # FILETIMEs after 31 Dec 9999 are truncated to max value
-        return MAX_FILETIME_EPOCH_DATETIME
+        return MAX_FILETIME_EPOCH_AS_UTC_DATETIME
     if epch < 0:
-        return FILETIME_EPOCH_AS_DATETIME
-    return FILETIME_EPOCH_AS_DATETIME + timedelta(microseconds=epch // 10)
+        return FILETIME_EPOCH_AS_UTC_DATETIME
+    return FILETIME_EPOCH_AS_UTC_DATETIME + timedelta(microseconds=epch // 10)
 
 
 FROZEN: bool = False
